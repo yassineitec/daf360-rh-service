c1da6042f537bfeb70677b98aeb4531487122db7
yassineitec <yassine.rhouma@itecgroupe.com>
2026-04-23 09:45:22 +0100

    chore: spring boot init

---
+++ src/main/java/com/daf360/rh/RhServiceApplication.java
@@ -0,0 +1,13 @@
+package com.daf360.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daf360/rh/RhServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daf360.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
